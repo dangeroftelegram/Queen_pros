bdb06f0674b2a719e745798033bd2c1ced20ae12
dangeroftelegram <67426127+dangeroftelegram@users.noreply.github.com>
2020-11-23 23:23:46 +0530

    Update afk.py

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -27,8 +27,8 @@ def afk(bot: Bot, update: Update):
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
-    update.effective_message.reply_text(tld(chat.id, f"{fname} എങ്ങോട് ആണ് ആവോ🧐🤔
- ഓ പോയത് പോയി🚶"))
+    update.effective_message.reply_text(tld(chat.id, f"{fname} എങ്ങോട് ആണ് ആവോ🧐🤔 ഓ പോയത് പോയി🚶"))
+ 
 
 
 @run_async
